1aa7fe7041b45d58faab3f921ef6d26a5541d6b4
Alfonso Jim <alfonsojim1897@gmail.com>
2021-03-17 16:52:18 +0100

    setState to method on VideoDetail prop

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 import youtube from "../components/apis/youtube";
 
 class App extends React.Component {
@@ -17,13 +18,14 @@ class App extends React.Component {
   };
 
   onVideoSelect = (video) => {
-    console.log("From the app!", video);
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={this.onVideoSelect}
           videos={this.state.videos}
